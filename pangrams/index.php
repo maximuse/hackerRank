13592f6a35d5c8e535ac0b10dbea908d11f85972
maximuse <maximuse@windowslive.com>
2020-06-20 11:33:44 +0200

    Initial commit

---
+++ pangrams/index.php
@@ -0,0 +1,31 @@
+<?php
+// Complete the pangrams function below.
+function pangrams($s) {
+    $s = mb_strtolower($s);
+    $alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    $letters = [];
+
+    for ($i = 0; $i < strlen($s); $i++) {
+        if (strpos($alphabet, $s[$i]) !== false) {
+            if (!in_array($s[$i], $letters)) {
+                $letters[] = $s[$i];
+            }
+        }
+    }
+
+    return (count($letters) == 26) ? 'pangram' : 'not pangram';
+}
+
+//$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("input00.txt", "r");
+
+$s = '';
+fscanf($stdin, "%[^\n]", $s);
+
+echo $result = pangrams($s);
+
+//fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+//fclose($fptr);
