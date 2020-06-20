7f86a7d1686c3c1afda30de96d57545dbeb55de2
maximuse <maximuse@windowslive.com>
2020-06-20 14:25:24 +0200

    Initial commit

---
+++ gradingStudents/index.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+function gradingStudents($grades) {
+    // Write your code here
+    $result = [];
+
+    foreach ($grades as $grade) {
+        if ($grade >= 38) {
+            $n = 0;
+
+            while (($grade + $n) % 5 != 0) {
+                $n++;
+            }
+
+            if ($n < 3) {
+                $result[] = $grade + $n;
+            }
+            else {
+                $result[] = $grade;
+            }
+        }
+        else {
+            $result[] = $grade;
+        }
+    }
+
+    return $result;
+}
+
+//$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("input00.txt", "r");
+$grades_count = intval(trim(fgets($stdin)));
+
+$grades = array();
+
+for ($i = 0; $i < $grades_count; $i++) {
+    $grades_item = intval(trim(fgets($stdin)));
+    $grades[] = $grades_item;
+}
+
+$result = gradingStudents($grades);
+var_dump($result);
+
+//fwrite($fptr, implode("\n", $result) . "\n");
+
+//fclose($fptr);
