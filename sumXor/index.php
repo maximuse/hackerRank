d30139010574f4c722a1850bce3326bf1941a214
maximuse <maximuse@windowslive.com>
2020-06-21 18:58:11 +0200

    Initial commit

---
+++ sumXor/index.php
@@ -0,0 +1,24 @@
+<?php
+// Complete the sumXor function below.
+function sumXor($n) {
+    $c = 0;
+
+    for ($i = 0; $i < $n; $i++) {
+        if (($n + $i) == (variant_xor($n, $i))) {
+            $c++;
+        }
+    }
+
+    return $c;
+}
+
+//$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("input00.txt", "r");
+$n = intval(trim(fgets($stdin)));
+
+echo $result = sumXor($n);
+
+//fwrite($fptr, $result . "\n");
+
+//fclose($fptr);
