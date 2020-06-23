4c8618524b41526e4e13d948b81dff3c2526028a
maximuse <maximuse@windowslive.com>
2020-06-23 14:24:42 +0200

    Initial commit

---
+++ catAndMouse/index.php
@@ -0,0 +1,37 @@
+<?php
+// Complete the catAndMouse function below.
+function catAndMouse($x, $y, $z) {
+    if (abs(($x - $z)) < abs(($y - $z))) {
+        return 'Cat A';
+    }
+    elseif (abs(($x - $z)) > abs(($y - $z))) {
+        return 'Cat B';
+    }
+    else {
+        return 'Mouse C';
+    }
+}
+
+//$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("input00.txt", "r");
+
+fscanf($stdin, "%d\n", $q);
+
+for ($q_itr = 0; $q_itr < $q; $q_itr++) {
+    fscanf($stdin, "%[^\n]", $xyz_temp);
+    $xyz = explode(' ', $xyz_temp);
+
+    $x = intval($xyz[0]);
+
+    $y = intval($xyz[1]);
+
+    $z = intval($xyz[2]);
+
+    $result = catAndMouse($x, $y, $z);
+
+    //fwrite($fptr, $result . "\n");
+}
+
+fclose($stdin);
+//fclose($fptr);
